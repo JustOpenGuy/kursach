bf57f10fc5bcdf31ba30a3f29032929b422d1c4f
Eugen <evgen6949@gmail.com>
2018-10-02 18:31:06 +0300

    Добавил общий каталог

---
+++ Source_JAVA/Project/src/sample/LessonController.java
@@ -0,0 +1,32 @@
+package sample;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class LessonController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button SignIn;
+
+    @FXML
+    private Button SignIn1;
+
+    @FXML
+    private Button SignIn2;
+
+    @FXML
+    private ImageView quess;
+
+    @FXML
+    void initialize() {
+    }
+}
