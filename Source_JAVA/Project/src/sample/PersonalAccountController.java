d9c81295df0e73d18ed0eba4d2ff0bf98d8b3388
Iaroslav Bezruchenko <yaagent38@gmail.com>
2018-10-04 00:56:47 +0300

    ЛК

--- Source_JAVA/Project/src/sample/PersonalAccountController.java
+++ Source_JAVA/Project/src/sample/PersonalAccountController.java
@@ -1,8 +1,14 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class PersonalAccountController {
     @FXML
@@ -23,5 +29,100 @@ public class PersonalAccountController {
     @FXML
     private MenuItem ChangePass;
 
+    @FXML
+    void initialize() {
 
+
+        LogOut.setOnAction(event -> {
+            GoToLesson.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("sample.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Parent rot = loader.getRoot();
+            loader.setRoot(rot);
+            Stage stage = new Stage();
+            stage.setScene(new Scene(rot));
+            stage.setResizable(false);
+            stage.sizeToScene();
+            stage.show();
+        }
+        );
+        About.setOnAction(event -> {
+
+                    FXMLLoader loader = new FXMLLoader();
+                    loader.setLocation(getClass().getResource("About.fxml"));
+                    try {
+                        loader.load();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Parent rot = loader.getRoot();
+                    loader.setRoot(rot);
+                    Stage stage = new Stage();
+                    stage.setScene(new Scene(rot));
+                    stage.setResizable(false);
+                    stage.sizeToScene();
+                    stage.show();
+                }
+        );
+        Information.setOnAction(event -> {
+
+                    FXMLLoader loader = new FXMLLoader();
+                    loader.setLocation(getClass().getResource("Inf.fxml"));
+                    try {
+                        loader.load();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Parent rot = loader.getRoot();
+                    loader.setRoot(rot);
+                    Stage stage = new Stage();
+                    stage.setScene(new Scene(rot));
+                    stage.setResizable(false);
+                    stage.sizeToScene();
+                    stage.show();
+                }
+        );
+        LogOut.setOnAction(event -> {
+                    GoToLesson.getScene().getWindow().hide();
+                    FXMLLoader loader = new FXMLLoader();
+                    loader.setLocation(getClass().getResource("sample.fxml"));
+
+                    try {
+                        loader.load();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Parent rot = loader.getRoot();
+                    loader.setRoot(rot);
+                    Stage stage = new Stage();
+                    stage.setScene(new Scene(rot));
+                    stage.setResizable(false);
+                    stage.sizeToScene();
+                    stage.show();
+                }
+        );
+        GoToLesson.setOnAction(event ->{GoToLesson.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/sample/Lesson.fxml"));
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setResizable(false);
+            stage.sizeToScene();
+            stage.showAndWait();
+        });
+    }
 }
+
+
