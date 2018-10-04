9091f397c948e3df87277454ab57acd0827fd220
Eugen <evgen6949@gmail.com>
2018-10-04 15:03:54 +0300

    ЛК

---
+++ Source_JAVA/Project/src/sample/Const.java
@@ -0,0 +1,10 @@
+package sample;
+
+
+public class Const {
+    public static final String USER_TABLE = "users"; //users - название созданной нами табличке в БД
+    public static final String USER_ID = "idusers"; //название созданной нами поля в БД
+    public static final String USER_USERNAME = "username";//название созданной нами поля в БД
+    public static final String USER_PASSWORD= "password";//название созданной нами поля в БД
+    public static final String USER_GENDER = " gender";//название созданной нами поля в БД
+}
